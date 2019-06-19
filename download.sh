a279f662c510f3fa065e009348bdb4790e0bb986
Oskari Timperi <oskari.timperi@iki.fi>
2019-06-19 20:06:26 +0300

    Remove download.sh

--- download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget https://www.python.org/ftp/python/3.5.1/python-3.5.1-amd64.exe
-cat unattend.xml.in | sed "s|TARGET_DIR|$PWD/Python-3.5.1-64|" > unattend.xml
-./python-3.5.1-amd64.exe /quiet /log install.log
