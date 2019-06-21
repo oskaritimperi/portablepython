37902c1aca190c96615d61d39f1b2b1ee9fa1f9b
Oskari Timperi <oskari.timperi@iki.fi>
2019-06-21 11:53:19 +0300

    First try downloading Python 2.X.Y

--- download.ps1
+++ download.ps1
@@ -1,7 +1,15 @@
-if ($env:PYARCH -eq "64") {
-    $filename = "python-${env:PYVERSION}-amd64.exe"
+if ($env:PYVERSION -like "3.*") {
+    if ($env:PYARCH -eq "64") {
+        $filename = "python-${env:PYVERSION}-amd64.exe"
+    } else {
+        $filename = "python-${env:PYVERSION}.exe"
+    }
 } else {
-    $filename = "python-${env:PYVERSION}.exe"
+    if ($env:PYARCH -eq "64") {
+        $filename = "python-${env:PYVERSION}.amd64.msi"
+    } else {
+        $filename = "python-${env:PYVERSION}.msi"
+    }
 }
 
 $url = "https://www.python.org/ftp/python/${env:PYVERSION}/${filename}"
@@ -17,10 +25,15 @@ Write-Output "Log file: $logfile"
 $client = New-Object System.Net.WebClient
 $client.DownloadFile($url, $target)
 
-# Replace TARGET_DIR in unattend.xml.in with our target directory
-((Get-Content -path unattend.xml.in -raw) -replace 'TARGET_DIR',$targetdir) | Set-Content -path unattend.xml
+if ($env:PYVERSION -like "3.*") {
+    # Replace TARGET_DIR in unattend.xml.in with our target directory
+    ((Get-Content -path unattend.xml.in -raw) -replace 'TARGET_DIR',$targetdir) | Set-Content -path unattend.xml
 
-Start-Process -FilePath "$target" -ArgumentList "/quiet","/log","$logfile" -Wait
+    Start-Process -FilePath "$target" -ArgumentList "/quiet","/log","$logfile" -Wait
 
-# Remove all __pycache__ directories
-Get-ChildItem -Include __pycache__ -Recurse -Force | Remove-Item -Force -Recurse
+    # Remove all __pycache__ directories
+    Get-ChildItem -Include __pycache__ -Recurse -Force | Remove-Item -Force -Recurse
+} else {
+    msiexec /qn /i $target TARGETDIR=$targetdir ADDLOCAL=DefaultFeature,TclTk,Documentation,Tools REMOVE=Extensions,Testsuite ALLUSERS=1
+    Get-ChildItem -Include "*.pyc" -Recurse -Force | Remove-Item -Force
+}
