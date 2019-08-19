9cabd54a5006d7ab68a8b631bd9f592bbf74b652
Oskari Timperi <oswjk@users.noreply.github.com>
2019-08-19 20:28:47 +0300

    Add logging to download.ps1

--- download.ps1
+++ download.ps1
@@ -22,6 +22,7 @@ Write-Output "Target: $target"
 Write-Output "Target dir: $targetdir"
 Write-Output "Log file: $logfile"
 
+Write-Output "Downloading $url"
 $client = New-Object System.Net.WebClient
 $client.DownloadFile($url, $target)
 
@@ -29,15 +30,21 @@ if ($env:PYVERSION -like "3.*") {
     # Replace TARGET_DIR in unattend.xml.in with our target directory
     ((Get-Content -path unattend.xml.in -raw) -replace 'TARGET_DIR',$targetdir) | Set-Content -path unattend.xml
 
+    Write-Output "Installing Python to $targetdir"
     Start-Process -FilePath "$target" -ArgumentList "/quiet","/log","$logfile" -Wait
 
     # Remove all __pycache__ directories
+    Write-Output "Removing __pycache__ directories"
     Get-ChildItem -Include __pycache__ -Recurse -Force | Remove-Item -Force -Recurse
 } else {
+    Write-Output "Installing Python to $targetdir"
     Start-Process -FilePath msiexec -ArgumentList "/qn","/i","$target","/L*V","$logfile","TARGETDIR=$targetdir","ADDLOCAL=DefaultFeature,TclTk,Documentation,Tools","REMOVE=Extensions,Testsuite" -Wait
+    
+    Write-Output "Removing .pyc files"
     Get-ChildItem -Include "*.pyc" -Recurse -Force | Remove-Item -Force
 }
 
 if (Test-Path $targetdir\Scripts) {
+    Write-Output "Removing $targetdir\Scripts"
     Remove-Item -Recurse -Force $targetdir\Scripts
 }
